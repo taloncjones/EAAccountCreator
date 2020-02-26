764b4241a8892e13ba584dff3d1c3c442c01c331
Talon Jones <taloncjones@gmail.com>
2020-02-26 13:17:23 -0800

    Split 'id','class' lookup into separate function

--- main.py
+++ main.py
@@ -52,14 +52,26 @@ class Browser:
   def goToURL(self, url):
     self.browser.get(url)
 
-  def checkFor(self, id, id_or_class='id'):
-    if id_or_class == 'id':
-      search = (By.ID, id)
-    elif id_or_class == 'class':
-      search = (By.CLASS_NAME, id)
-    else:
-      sys.exit('Error: checkFor id_or_class value invalid')
+  typeSelector = {
+    'id': By.ID,
+    'class': By.CLASS_NAME,
+    'xpath': By.XPATH,
+  }
 
+  def byLookup(self, lookupType):
+    '''Returns By.ID from typeSelector based on lookupType. If lookupType not found in typeSelector, sys.exit() with error.'''
+    selected = self.typeSelector.get(lookupType)
+    if selected: return selected
+    else: sys.exit('Error: Invalid lookupType')
+
+
+  def checkFor(self, id, lookupType='id'):
+    '''
+    Checks for element where lookupType = id
+    id (str): What to look for on page
+    [lookupType] (str): What <id> is. e.g. 'id', 'class'
+    '''
+    search = (self.byLookup(lookupType), id)
     try:
       return self.browser.find_element(*search).is_displayed()
     except NoSuchElementException:
