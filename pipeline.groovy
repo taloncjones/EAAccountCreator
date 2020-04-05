2bebf342591648690725ddd02e50edf05c663530
Josh Vaughen <vaughen@adobe.com>
2020-04-04 17:00:49 -0700

    update chromedriver path

--- pipeline.groovy
+++ pipeline.groovy
@@ -20,7 +20,7 @@ pipeline {
               withCredentials([string(credentialsId: 'eaaccountcreatorsheet', variable: 'eaaccountcreatorsheet')]) {
                 sh """
                 	pip3 install -r requirements.txt
-                    python3 main.py 'chrome' 'chromedriver' 'accounts.json' \$eaaccountcreatorsheet 'email.json'
+                    python3 main.py 'chrome' '/var/lib/jenkins/workspace/eaaccountcreator/chromedriver' 'accounts.json' \$eaaccountcreatorsheet 'email.json'
                 """
             }
           }
